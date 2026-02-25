864cd39919dc327d8c422f336e9a22d4131bda83
LookAtFr3sn0 <lookatfr3sn0@gmail.com>
2026-02-25 19:50:56 +0100

    feat: add Bundle type

--- src/types/parameters.ts
+++ src/types/parameters.ts
@@ -3,3 +3,33 @@ export type hash = 'sha256' | 'sha512';
 export type info = string;
 export type pqkem = 'Crystal-Kyber-512' | 'Crystal-Kyber-768' | 'Crystal-Kyber-1024';
 export type aead = 'AES-128-GCM' | 'AES-256-GCM' | 'ChaCha20-Poly1305';
+
+export type Bundle = {
+    IK: {
+        type: curve;
+        publicKey: Uint8Array;
+    },
+    SPK: {
+        id: number;
+        type: curve;
+        publicKey: Uint8Array;
+        signature: Uint8Array;
+    },
+    lastResort: {
+        id: number;
+        type: pqkem;
+        publicKey: Uint8Array;
+        signature: Uint8Array;
+    },
+    oneTimeCurveKeys: Array<{
+        id: number;
+        type: curve;
+        publicKey: Uint8Array;
+    }>,
+    oneTimePQKEMKeys: Array<{
+        id: number;
+        type: pqkem;
+        publicKey: Uint8Array;
+        signature: Uint8Array;
+    }>
+}
