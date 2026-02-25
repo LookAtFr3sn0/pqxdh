b2e6c4bfac2b97549a17ce8f064de0a1872142bd
LookAtFr3sn0 <lookatfr3sn0@gmail.com>
2026-02-25 18:58:14 +0100

    feat: implement EncodeKEM and DecodeKEM functions for KEM encoding and decoding

---
+++ src/core/KEM.ts
@@ -0,0 +1,26 @@
+import type { pqkem } from "../types/parameters.js";
+
+export function EncodeKEM(type: pqkem, publicKey: Uint8Array): Uint8Array {
+    if (type === 'Crystal-Kyber-512' && publicKey.length === 800) {
+        let bytes = new Uint8Array([0x03, ...publicKey]);
+        return bytes;
+    } else if (type === 'Crystal-Kyber-768' && publicKey.length === 1184) {
+        let bytes = new Uint8Array([0x04, ...publicKey]);
+        return bytes;
+    } else if (type === 'Crystal-Kyber-1024' && publicKey.length === 1568) {
+        let bytes = new Uint8Array([0x05, ...publicKey]);
+        return bytes;
+    }
+    throw new Error('Invalid KEM type or public key length');
+}
+
+export function DecodeKEM(encoded: Uint8Array): { type: pqkem, publicKey: Uint8Array } {
+    if (encoded[0] === 0x03 && encoded.length === 801) {
+        return { type: 'Crystal-Kyber-512', publicKey: encoded.slice(1) };
+    } else if (encoded[0] === 0x04 && encoded.length === 1185) {
+        return { type: 'Crystal-Kyber-768', publicKey: encoded.slice(1) };
+    } else if (encoded[0] === 0x05 && encoded.length === 1569) {
+        return { type: 'Crystal-Kyber-1024', publicKey: encoded.slice(1) };
+    }
+    throw new Error('Invalid KEM encoding');
+}
