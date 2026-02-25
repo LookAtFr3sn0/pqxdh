a4e65a1b87c659ca43ddaf30843ea56e0ed22cfe
LookAtFr3sn0 <lookatfr3sn0@gmail.com>
2026-02-25 18:33:36 +0100

    feat: add TypeScript types for cryptographic parameters

---
+++ src/types/parameters.ts
@@ -0,0 +1,5 @@
+export type curve = 'curve25519' | 'curve448';
+export type hash = 'sha256' | 'sha512';
+export type info = string;
+export type pqkem = 'Crystal-Kyber-512' | 'Crystal-Kyber-768' | 'Crystal-Kyber-1024';
+export type aead = 'AES-128-GCM' | 'AES-256-GCM' | 'ChaCha20-Poly1305';
