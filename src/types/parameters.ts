40dd0bb68e223c0cec41e7b69f1af6baed98773a
LookAtFr3sn0 <lookatfr3sn0@gmail.com>
2026-02-26 02:26:07 +0100

    feat: update pqkem types to use ML-KEM naming

--- src/types/parameters.ts
+++ src/types/parameters.ts
@@ -1,7 +1,7 @@
 export type curve = 'curve25519' | 'curve448';
 export type hash = 'sha256' | 'sha512';
 export type info = string;
-export type pqkem = 'Crystal-Kyber-512' | 'Crystal-Kyber-768' | 'Crystal-Kyber-1024';
+export type pqkem = 'ML-KEM-512' | 'ML-KEM-768' | 'ML-KEM-1024';
 export type aead = 'AES-128-GCM' | 'AES-256-GCM' | 'ChaCha20-Poly1305';
 
 export type Bundle = {
